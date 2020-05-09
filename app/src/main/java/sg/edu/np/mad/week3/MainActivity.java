c764efe66b43163abc81a49b6ba09b5a1a386cf4
jw <wyipjun@gmail.com>
2020-05-09 20:42:39 +0800

    practical finished

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -3,6 +3,11 @@ package sg.edu.np.mad.week3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView babyPic =findViewById(R.id.babyimageview);
+        babyPic.setImageURI(
+                Uri.parse("android.resource://" +getPackageName() + "/" +R.raw.cute_baby)
+        );
+        
+
+        VideoView rabbitVideo = findViewById(R.id.RabbitVideo);
+        rabbitVideo.setVideoURI(
+                Uri.parse("android.resource://" +getPackageName() + "/" +R.raw.rabbid)
+        );
+        rabbitVideo.start();
     }
 }
